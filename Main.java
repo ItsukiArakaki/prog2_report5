cc7ac053581be145b08ff9083ba2edd5b9a7b834
ItsukiArakaki <e215746@ie.u-ryukyu.ac.jp>
2022-02-08 00:44:59 +0900

    step2

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 class Main{
-    public static void main(String[] args){
-            String str = null;
+    public static void main(String[] args) {
+        String str = null;
+        try{   
             System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生");
+            System.out.println(e.getMessage());
+        }    
     }
+    
 }
